c96eb0a3c348dfa0f633e0c109539449940fcb0e
SureshKrishna-27 <sureshkrishna2706@gmail.com>
2024-12-29 22:43:10 +0530

    Initial

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Frontend Developer","Java Developer", "web Developer"],
+    typeSpeed:100,
+    backSpeed: 100,
+    backDeley:1000,
+    loop:true
+})
